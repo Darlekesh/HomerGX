4a0d8cfde2225f090d302f8ca00f563a3830cc48
Darlekesh <lubomir@gabrys.cz>
2022-04-04 01:07:32 +0200

    Added Portainer service

---
+++ src/components/services/Portainer.vue
@@ -0,0 +1,72 @@
+<template>
+  <Generic :item="item">
+    <template #content>
+      <p class="title is-4">{{ item.name }}</p>
+      <p class="subtitle is-6">
+        <template v-if="item.subtitle">
+          {{ item.subtitle }}
+        </template>
+        <template v-else-if="onlinec">
+          {{ onlinec }} {{ offlinec}}
+        </template>
+      </p>
+    </template>
+  </Generic>
+</template>
+
+<script>
+import service from "@/mixins/service.js";
+import Generic from "./Generic.vue";
+
+export default {
+  name: "Portainer",
+  mixins: [service],
+  props: {
+    item: Object,
+  },
+  components: {
+    Generic,
+  },
+  data: () => ({
+    api: null,
+  }),
+  computed: {
+    onlinec: function () {
+      if (this.online) {
+        return this.online.toFixed(1);
+      }
+      return "";
+    },
+    offlinec: function () {
+      if (this.offline) {
+        return this.offline.toFixed(1);
+      }
+      return "";
+    },
+  },
+  created() {
+    this.fetchStatus();
+  },
+  methods: {
+    fetchStatus: async function () {
+      if (this.item.subtitle != null) return;
+
+      const apikey = this.item.apikey;
+      if (!apikey) {
+        console.error(
+          "apikey is not present in config.yml for the portainer entry!"
+        );
+        return;
+      }
+      const result = await this.fetch("/api/endpoints/2?all=true", {
+        headers: {
+          'X-API-Key': + this.item.apikey,
+        },
+      }).catch((e) => console.log(e));
+
+      this.online = result.Snapshots.RunningContainerCount;
+      this.offline = result.Snapshots.StoppedContainerCount;
+    },
+  },
+};
+</script>
